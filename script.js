90aea7d3052330ed008d24047a6c91c5ca09700b
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2025-01-13 11:26:29 -0500

    Modify HTML and JS
    Modified index.html to add a book list div where the book cards will be inserted via javascript
    Modified script.js to add a function to loop through the myLibrary array to get the attributes of each book object and display them in a card div.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let bookList = document.querySelector('.book-list')
 const myLibrary = [];
 
+// book constructor function with read attribute being true or false
 function Book(title, author, pageCount, read) {
     this.title = title;
     this.author = author;
@@ -7,15 +9,35 @@ function Book(title, author, pageCount, read) {
     this.read = read;
 }
 
-const book = new Book('Crime and Punishment', 'Fyodor Dostoevsky', 270, false);
+// manual entries to test things
+const crimeAndPunishment = new Book('Crime and Punishment', 'Fyodor Dostoevsky', 270, false);
+const theBrothersKaramazov = new Book('The Brothers Karamazov', 'Fyodor Dostoevsky', 270, false)
+const notesFromUnderground = new Book('Notes From Underground', 'Fyodor Dostoevsky', 270, false)
+console.log(crimeAndPunishment.title, crimeAndPunishment.author, crimeAndPunishment.pageCount, crimeAndPunishment.read);
 
-console.log(book.title, book.author, book.pageCount, book.read);
-
-
+// function to append books to library array
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-addBookToLibrary(book);
-
+// test for seeing if the book is appended to the array
+addBookToLibrary(crimeAndPunishment);
+addBookToLibrary(theBrothersKaramazov);
+addBookToLibrary(notesFromUnderground);
 console.log(myLibrary);
+
+
+// function to loop through myLibrary array and print each book's contents on to the page
+function getBooks(library) {
+    for (let index = 0; index < library.length; index++) {
+        let book = library[index];
+        console.log(book.title);
+        let bookCard = document.createElement('div');
+        bookList.appendChild(bookCard);
+        bookCard.innerHTML = `<h2> ${book.title} </h2><h4> ${book.author} </h4><p> ${book.pageCount} </p><p> ${book.read} </p>`;
+
+
+    }
+}
+
+getBooks(myLibrary);
