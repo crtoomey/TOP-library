8121f9f159068c1a30156bdcc563283b52b9a53b
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2025-01-13 14:37:10 -0500

    Modify HTML, CSS, and JS
    Modified index.html to add placeholder text for when the user has not added a book yet.
    Modified script.js to check if a new book already exists in the array using the index.some() function and altered the getBooks() function to clear the list each time it is called
    so that there were no duplicate items when someone submitted the add book form.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("#close");
+const submitButton = document.querySelector('#submit')
 let bookList = document.querySelector('.book-list')
 const myLibrary = [];
 
@@ -10,31 +11,53 @@ function Book(title, author, pageCount, read) {
     this.author = author;
     this.pageCount = pageCount;
     this.read = read;
+    this.info = function () {
+        if (this.read === false) {
+            return `${this.title} by ${this.author}, ${this.pageCount} pages, not read yet.`
+        } else {
+            return `${this.title} by ${this.author}, ${this.pageCount} pages, has been read.`
+        }
+    }
 }
 
 // manual entries to test things
-const crimeAndPunishment = new Book('Crime and Punishment', 'Fyodor Dostoevsky', 270, false);
-const theBrothersKaramazov = new Book('The Brothers Karamazov', 'Fyodor Dostoevsky', 270, false)
-const notesFromUnderground = new Book('Notes From Underground', 'Fyodor Dostoevsky', 270, false)
-console.log(crimeAndPunishment.title, crimeAndPunishment.author, crimeAndPunishment.pageCount, crimeAndPunishment.read);
+// const crimeAndPunishment = new Book('Crime and Punishment', 'Fyodor Dostoevsky', 270, true);
+// const theBrothersKaramazov = new Book('The Brothers Karamazov', 'Fyodor Dostoevsky', 270, false);
+// const notesFromUnderground = new Book('Notes From Underground', 'Fyodor Dostoevsky', 270, false);
+// console.log(crimeAndPunishment.info(), theBrothersKaramazov.info(), notesFromUnderground.info());
+// console.log(crimeAndPunishment.title, crimeAndPunishment.author, crimeAndPunishment.pageCount, crimeAndPunishment.read);
 
 // function to append books to library array
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    // check to see if the new book already exists in the array
+    const exists = myLibrary.some(
+        (bk) =>
+            bk.title === book.title && bk.author === book.author
+    );
+
+    if (exists) {
+        console.log('Book already exists in the library');
+    } else {
+        console.log('adding book');
+        myLibrary.push(book);
+    }
+
+
 }
 
 // test for seeing if the book is appended to the array
-addBookToLibrary(crimeAndPunishment);
-addBookToLibrary(theBrothersKaramazov);
-addBookToLibrary(notesFromUnderground);
-console.log(myLibrary);
+// addBookToLibrary(crimeAndPunishment);
+// addBookToLibrary(theBrothersKaramazov);
+// addBookToLibrary(notesFromUnderground);
+// console.log(myLibrary);
 
 
 // function to loop through myLibrary array and print each book's contents on to the page
 function getBooks(library) {
+    bookList.innerHTML = "";
     for (let index = 0; index < library.length; index++) {
         let book = library[index];
-        console.log(book.title);
+
         let bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
         bookList.appendChild(bookCard);
@@ -42,15 +65,10 @@ function getBooks(library) {
         <h4 class="book-author"> ${book.author} </h4>
         <p class="page-count"> ${book.pageCount} </p>
         <p class="read"> ${book.read} </p>`;
-
-
     }
 }
 
-getBooks(myLibrary);
-
-
-// show add book dialog modal
+// show add book dialog modal from the internet
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -60,3 +78,20 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
+
+// submit form function
+submitButton.addEventListener("click", submitForm, false);
+
+function submitForm(event) {
+    event.preventDefault();
+    let bookTitle = document.querySelector('#title').value;
+    let bookAuthor = document.querySelector('#author').value;
+    let bookPageCount = document.querySelector('#page-count').value;
+    const newBook = new Book(bookTitle, bookAuthor, bookPageCount, false);
+    console.log(newBook.title);
+    addBookToLibrary(newBook);
+    // console.log(myLibrary)
+    getBooks(myLibrary);
+    dialog.close();
+}
+
